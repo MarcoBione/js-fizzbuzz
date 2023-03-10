29729e91b269fe0c14ff60b088f746c3333828a8
MarcoBione <marcobione.work@gmail.com>
2023-03-10 15:38:31 +0100

    Add stamp props in JS + style @ fizzbuzz boxes

---
+++ JS/script.js
@@ -0,0 +1,70 @@
+/*
+*Consegna:*
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*Prima di partire a scrivere codice poniamoci qualche domanda:*
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+*Consigli del giorno:*
+1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*BONUS 1:*
+Crea un container nel DOM , aggiungendo (attraverso la funzione append() o innerHTML) un elemento html con il numero o la stringa corretta da mostrare.
+*BONUS 2:*
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
+*/
+
+for (let i = 0; i <= 100; i++){
+        
+    //cycle stamp condition
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log('FizzBuzz');
+
+        //stamp box fizzbuzz
+        document.getElementsById('display').innerHTML +=`
+        <div class="fizzbuzz">
+            <p>FizzBuzz</p>
+            <hr>
+            <p>${i}</p>
+        </div>
+        `;
+
+    }else if (i % 3 == 0){
+        console.log('Fizz');
+
+        //stamp box fizz
+        document.getElementsById('display').innerHTML +=`
+        <div class="fizz">
+            <p>Fizz</p>
+            <hr>
+            <p>${i}</p>
+        </div>
+        `;
+
+    }else if (i % 5 == 0){
+        console.log('Buzz');
+
+        //stamp box buzz
+        document.getElementsById('display').innerHTML +=`
+        <div class="buzz">
+            <p>Buzz</p>
+            <hr>
+            <p>${i}</p>
+        </div>
+        `;
+    }else {
+        console.log(i);
+        //stamp box buzz
+        document.getElementsById('display').innerHTML +=`
+        <div class="buzz">
+            <p>Buzz</p>
+            <hr>
+            <p>${i}</p>
+        </div>
+        `;
+    }
+}
+
